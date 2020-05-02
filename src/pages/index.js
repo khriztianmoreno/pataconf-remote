c1bdbcb8f2df1313e98fc521bfb4baf8a2b4c913
khriztianmoreno <khriztianmoreno@gmail.com>
2020-05-02 18:41:43 -0500

    Add the gallery section

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,182 @@ const IndexPage = () => (
     <About />
     <Speakers />
     <TicketPrices />
+    <section id="gallery-section" className="pa-100 pb-0">
+      <div className="container-fluid p-0">
+
+
+        <div className="section-intro text-center mr-bottom-70">
+          <h2>Gallery</h2>
+          <p className="sub-title color-gray">Captured Event Moments</p>
+          <hr className="ev-line ev-line-c5" />
+        </div>
+
+        <div id="js-grid-masonry" className="cbp">
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x570/eee/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/2ad2f3" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x360/ccc/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/8d285b" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x570/eee/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/3ad2f3" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x360/ccc/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/8d285b" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x570/eee/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/2ad2f3" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x570/bbb/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/8d285b" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x570/bbb/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/2ad2f3" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x360/aaa/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/8d285b" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x360/aaa/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/2ad2f3" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="cbp-item">
+            <div className="cbp-caption">
+              <div className="cbp-caption-defaultWrap">
+                <img src="https://via.placeholder.com/380x360/aaa/ddd" alt="Event" />
+              </div>
+              <div className="cbp-caption-activeWrap">
+                <div className="cbp-l-caption-alignCenter">
+                  <div className="cbp-l-caption-body">
+                    <a href="http://scriptpie.com/cubeportfolio/img/?i=1200x900/8d285b" className="cbp-lightbox"
+                      data-title="Event Photos"><i className="flaticon-zoom-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
