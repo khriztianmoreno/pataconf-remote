42626167461c7bf5dd8a9156f7dfc727e8f2569e
khriztianmoreno <khriztianmoreno@gmail.com>
2020-05-02 14:58:47 -0500

    Add the sponsor section

---
+++ src/components/Sponsors.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sponsors = () => (
+  <div id="sponsor-section">
+    <div className="container">
+      <div className="shadow__depth1 sponsor-wrap d-md-flex flex-row justify-content-between align-items-center">
+        <img src="assets/img/sp1.png" alt="sponsor" className="img-fluid" />
+        <img src="assets/img/sp1.png" alt="sponsor" className="img-fluid" />
+        <img src="assets/img/sp1.png" alt="sponsor" className="img-fluid" />
+        <img src="assets/img/sp1.png" alt="sponsor" className="img-fluid" />
+        <img src="assets/img/sp1.png" alt="sponsor" className="img-fluid" />
+      </div>
+    </div>
+  </div>
+)
+
+export default Sponsors
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import Hero from '../components/Hero'
+import Sponsors from '../components/Sponsors'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Sponsors />
   </Layout>
 )
 
