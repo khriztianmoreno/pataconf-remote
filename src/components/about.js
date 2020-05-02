5853adff68993ecf68537fd92acc22493a61dd0d
Mafe Serna <mafesernaarboleda@gmail.com>
2020-05-02 17:04:02 -0500

    add about section

---
+++ src/components/about.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const About = () => (
+  <section id="about-section">
+    <div className="container">
+      <div className="row d-flex align-items-center">
+        <div className="col-md-6">
+          <h2>
+              What is Business
+              Conference 2020
+              is All About
+          </h2>
+          <p className="sub-title color-gray">Where The Business World Meets</p>
+        </div>
+        <div className="col-md-6">
+          <p>
+            Websites in professional use templating systems. Commercial publishing platforms and content
+            management systems ensure that you can show different text, different data using the same about
+            controlling hundreds of articles, profiles in social networks.
+          </p>
+          <p>
+            Content management systems ensure that you can show different text, ferent data using the same
+            about controlling hundreds of articles, profiles social networks.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <About />
   </Layout>
 )
 
