d2581352a85d4a1f95dcc6763c7e97e1f91df55d
khriztianmoreno <khriztianmoreno@gmail.com>
2020-05-02 23:19:52 -0500

    Remove preloader component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Preloader from './Prealoader'
 import Header from "./Header"
 import Footer from "./Footer"
-
-const ENV = process.env.NODE_ENV
 
 const Layout = ({ children }) => {
   return (
@@ -13,7 +10,6 @@ const Layout = ({ children }) => {
       <a href="/" id="top-button">
         <i className="fas fa-chevron-up"></i>
       </a>
-      {ENV === 'production' && <Preloader /> }
       <Header />
       {children}
       <Footer />
